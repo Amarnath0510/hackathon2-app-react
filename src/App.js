f970cefe6805d4cd61ce96ec88bbdb674ddbd262
Amarnath0510 <amarmuthu007@gmail.com>
2022-01-31 17:39:55 +0530

    react finished

--- src/App.js
+++ src/App.js
@@ -1,78 +1,34 @@
 
 import './App.css';
-import Card from '@mui/material/Card';
+import AppBar from '@mui/material/AppBar';
+
+import Toolbar from '@mui/material/Toolbar';
+import { Switch,Route,useHistory } from "react-router-dom"
+import Button from '@mui/material/Button';
+
+import Product from "./products.js";
+
 
 function App() {
-  const products=[
-   { 
-
-     id:"100",
-     image:"https://static.toiimg.com/thumb/resizemode-4,msid-79729969,width-1200,height-900/79729969.jpg",
-    name:"Iphone 13 pro",
-    ratings:"⭐⭐⭐⭐3357",
-    price:"₹1,35,800",
-    
-
-  },
-
-  { 
-    id:"102",
-    image:"https://i1.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/03/Samsung-Galaxy-M12-review-5-scaled.jpg?ssl=1",
-  name:"Samsung galaxy m12",
-  ratings:"⭐⭐⭐⭐83,457",
-  price:"₹11,658",
-
-
-},
-
-
-{ id:"103",
-  image:"https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg",
-name:"Iphone 11",
-ratings:"⭐⭐⭐⭐5621",
-price:"₹54,900",
-
-
-},
-
-
-{   
-  id:"104",
-   image:"https://dealntechcdn.b-cdn.net/wp-content/uploads/2021/08/vivo-Y53-4G-460x460.jpg",
-name:"Vivo Y53",
-ratings:"⭐⭐⭐⭐260",
-price:"₹16,896",
-
-
-},
-
-{
-  id:"105",
-   image:"https://www.kickmobiles.com/images/thumbs/0020854_xiaomi-redmi-9a_808.jpeg",
-name:"Redmi 9A",
-ratings:"⭐⭐⭐⭐6987",
-price:"₹23000",
-
-
-}
-
- ];
+const history=useHistory("");
   return (
     <div className="App">
-     {products.map((product,index)=>(
-      <Card variant="outlined" key={index} className="product-container">
-     
-<img  className="product-pic"
-src={product.image}
-alt={product.name}
-/>
- <p className="product-name">{product.name}</p>
- <p>{product.ratings}</p>
- <p>{product.price}</p>
+    <AppBar position="static">
+    <Toolbar variant="dense">
+   
+    <Button variant="text" color="inherit" onClick={()=>history.push("/products")}>PRODUCTS</Button>
+    </Toolbar>
+    </AppBar>
+  <Switch>
  
+ <Route  path= "/products">
+ <Product/>
+ </Route>
+  </Switch>
+    
+    
+   
 
-</Card>
-     ))}
    
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,89 @@
+import React from "react";
+import Card from '@mui/material/Card';
+
+function Product(){
+    const products=[
+        { 
+     
+          id:"100",
+          image:"https://static.toiimg.com/thumb/resizemode-4,msid-79729969,width-1200,height-900/79729969.jpg",
+         name:"Iphone 13 pro",
+         ratings:"⭐⭐⭐⭐3357",
+         price:"₹1,35,800",
+         
+     
+       },
+     
+       { 
+         id:"102",
+         image:"https://i1.wp.com/www.smartprix.com/bytes/wp-content/uploads/2021/03/Samsung-Galaxy-M12-review-5-scaled.jpg?ssl=1",
+       name:"Samsung Galaxy m12",
+       ratings:"⭐⭐⭐⭐83,457",
+       price:"₹11,658",
+     
+     
+     },
+     
+     
+     { id:"103",
+       image:"https://m.media-amazon.com/images/I/71i2XhHU3pL._SL1500_.jpg",
+     name:"Iphone 11",
+     ratings:"⭐⭐⭐⭐5621",
+     price:"₹54,900",
+     
+     
+     },
+     
+     
+     {   
+       id:"104",
+        image:"https://dealntechcdn.b-cdn.net/wp-content/uploads/2021/08/vivo-Y53-4G-460x460.jpg",
+     name:"Vivo Y53",
+     ratings:"⭐⭐⭐⭐260",
+     price:"₹16,896",
+     
+     
+     },
+     
+     {
+       id:"105",
+        image:"https://www.kickmobiles.com/images/thumbs/0020854_xiaomi-redmi-9a_808.jpeg",
+     name:"Redmi 9A",
+     ratings:"⭐⭐⭐⭐6987",
+     price:"₹23000",
+     
+     
+     }
+     
+      ];
+
+
+// useEffect(() => {
+//  fetch("http://localhost:2000/products")
+//   .then((data)=>data.json())
+//   .then((products) => setProducts(products))
+
+// },[]);
+
+
+    return(
+        <div className="container">
+        {products.map((product,index)=>(
+            <Card variant="outlined" key={index} className="product-container">
+           
+      <img  className="product-pic"
+      src={product.image}
+      alt={product.name}
+      />
+       <p className="product-name">{product.name}</p>
+       <p>{product.ratings}</p>
+       <p>{product.price}</p>
+       
+      
+      </Card>
+           ))}
+           </div>
+           
+    )
+}
+export default Product;
